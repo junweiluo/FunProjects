2e2e8fd4c507b24c94a47ba8a829351da9ab2369
Junwei Luo <junwei.luo9777@gmail.com>
2019-12-28 22:37:03 -0500

    add comments

--- RotateMatrix-RawPython.py
+++ RotateMatrix-RawPython.py
@@ -1,3 +1,5 @@
+# This is using raw python to solve this problem.
+
 OriginalGrid = [[".", ".", ".", ".", ".", "."],
         [".", "0", "0", ".", ".", "."],
         ["0", ".", "0", "0", ".", "."],
